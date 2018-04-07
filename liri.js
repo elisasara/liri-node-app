0749e2c304489cfba927374d72741309843bfca5
Elisa Urda <urda.elisa@gmail.com>
2018-04-07 18:05:22 -0400

    created spotify function

--- liri.js
+++ liri.js
@@ -2,34 +2,56 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 var twitter = require("twitter");
-// var spotify = require("node-spotify-api")
-// var spotify = new spotify(keys.spotify);
+var spotify = require("node-spotify-api")
+var spot = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
-var params = {screen_name: 'Elisa_Penn18'}
 
 
 
 switch (process.argv[2]) {
-    case "my-tweets": 
-    getTweets();
-    break;
+    case "my-tweets":
+        getTweets();
+        break;
     case "spotify-this-song":
+        getSong();
 
-    break;
+        break;
     case "movie-this":
 
-    break;
+        break;
     case "do-what-it-says":
 
-};  
+};
 
-function getTweets (){
-    client.get("statuses/user_timeline", params, function (error, response, tweets){
-            console.log (tweets);
-            for (var i = 0; i<tweets.length; i++){
-                console.log(JSON.parse(tweets[i]).created_at);
-                console.log(JSON.parse(tweets[i]).text);
-            };
+function getTweets() {
+    var params = { screen_name: 'Elisa_Penn18' };
+
+    client.get("statuses/user_timeline", params, function (error, response, tweets) {
+        // console.log(tweets.body);
+        // for (var i = 0; i < 20; i++) {
+            console.log(tweets.body[0].created_at);
+            console.log(tweets.body[0].text);
+        // };
     });
 };
 
+function getSong() {
+    var song = process.argv.slice(3).join(" ");
+    if (process.argv.length < 4) {
+        song = "The Sign";
+    };
+
+    spot.search({ type: "track", query: song })
+        .then(function (response) {
+            var info = response.tracks.items;
+            // console.log(info);
+
+            console.log("Artist(s): " + info[0].artists);
+
+            console.log("Song Name: " + info[0].name);
+
+            console.log("Link: " + info[0].external_urls.spotify);
+
+            console.log("Album Name: " + info[0].album.name);
+        });
+};
